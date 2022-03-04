e963f0188f0af41cbb6e9b70e52fa7f2d863c7a2
Jack Collins <jackcollins@Jacks-MacBook-Air.local>
2022-03-03 22:29:39 -0800

    fix warnings

--- src/components/auth/RequestForgotPassword.jsx
+++ src/components/auth/RequestForgotPassword.jsx
@@ -18,14 +18,14 @@ const RequestForgotPassword = () => {
     })
     .then(response => response.json())
     .then(data => {
-      if (data.status == 200) {
+      if (data.status === 200) {
         setMessage(data.message)
       }
     });
   }
 
   const apiMessage = () =>{
-    if (message != ''){
+    if (message !== ''){
       return (
         <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mt-4" role="alert">
           <span className="block sm:inline">{message}.</span>
