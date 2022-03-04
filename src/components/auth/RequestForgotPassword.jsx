c4f61e07c7cc68ec2a9c83d285018bba6ac7738b
Jack Collins <jackcollins@Jacks-MacBook-Air.local>
2022-03-03 22:32:46 -0800

    fix

--- src/components/auth/RequestForgotPassword.jsx
+++ src/components/auth/RequestForgotPassword.jsx
@@ -28,7 +28,7 @@ const RequestForgotPassword = () => {
     if (message !== ''){
       return (
         <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mt-4" role="alert">
-          <span className="block sm:inline">{message}.</span>
+          <span className="block sm:inline"><p>{message}</p></span>
         </div>
       )
     }
